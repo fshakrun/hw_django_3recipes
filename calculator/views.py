bdc0b886d4f7e8daca200b05223108968401bd75
Fedor Shakrun <fshakrun@gmail.com>
2022-10-17 14:06:15 +0300

    initial commit

---
+++ calculator/views.py
@@ -0,0 +1,73 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+    # можете добавить свои рецепты ;)
+}
+
+
+# Напишите ваш обработчик. Используйте DATA как источник данных
+# Результат - render(request, 'calculator/index.html', context)
+# В качестве контекста должен быть передан словарь с рецептом:
+# context = {
+#   'recipe': {
+#     'ингредиент1': количество1,
+#     'ингредиент2': количество2,
+#   }
+# }
+
+def omelet_calculate(request, recipe):
+    if recipe in DATA.keys():
+        servings = request.GET.get('servings', 1)
+        ingredients = {}
+        for ingredient in DATA[recipe]:
+            ingredients[ingredient] = round(DATA[recipe][ingredient] * int(servings), 2)
+        context = {
+            'recipe': ingredients
+        }
+    else:
+        context = {}
+    return render(request, 'calculator/index.html', context)
+
+
+def pasta_calculate(request, recipe):
+    if recipe in DATA.keys():
+        servings = request.GET.get('servings', 1)
+        ingredients = {}
+        for ingredient in DATA[recipe]:
+            ingredients[ingredient] = round(DATA[recipe][ingredient] * int(servings), 2)
+        context = {
+            'recipe': ingredients
+        }
+    else:
+        context = {}
+    return render(request, 'calculator/index.html', context)
+
+
+def buter_calculate(request, recipe):
+    if recipe in DATA.keys():
+        servings = request.GET.get('servings', 1)
+        ingredients = {}
+        for ingredient in DATA[recipe]:
+            ingredients[ingredient] = round(DATA[recipe][ingredient] * int(servings), 2)
+        context = {
+            'recipe': ingredients
+        }
+    else:
+        context = {}
+    return render(request, 'calculator/index.html', context)
